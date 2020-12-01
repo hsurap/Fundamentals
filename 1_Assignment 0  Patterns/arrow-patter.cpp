8eea29ec4ed65c76c29577ae7735cb6f2b44cf88
parush2305 <parush.p2305@gmail.com>
2020-12-01 18:21:13 +0530

    medium

---
+++ 1_Assignment 0  Patterns/arrow-patter.cpp
@@ -0,0 +1,32 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n-n/2;i++)
+    {
+        for(int j=i-1;j>0;j--)
+        {
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+    for(int i=1;i<=n/2;i++)
+    {
+        for(int j=1;j<=n/2-i;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=i;j<=n/2;j++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+}
