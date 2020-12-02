75fe160ad2a173f941f44dd82787f46e26688f21
parush2305 <parush.p2305@gmail.com>
2020-12-02 23:59:08 +0530

    medium

---
+++ 3_Lecture 2  Arrays & Strings/merge-two-sorted-array.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[100], b[100],c[200];
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0;i<m;i++)
+    {
+        cin>>b[i];
+    }
+    int i=0,j=0,k=1;
+    while(i<n&&j<m)
+    {
+        if(a[i]<=b[j])
+        {
+            c[k]=a[i];
+            k++;i++;
+        }
+        else{
+            c[k]=b[j];
+            k++;j++;
+        }
+    }
+    while(i<n)
+    {
+        c[k]=a[i];
+        i++;k++;
+    }
+    while(j<m)
+    {
+        c[k]=a[j];
+        j++;k++;
+    }
+
+
+}
