e70ed7e883c05c9a4a5919dbb7ebcc42115827b9
parush2305 <parush.p2305@gmail.com>
2020-12-07 21:57:38 +0530

    easy

---
+++ 8_Lecture 5  Recursion 1/array-sorted-r-not.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+bool issorted(int a[],int n)
+{
+    if(n==1){return true;}
+    if((a[0]<a[1])&&issorted(a+1,n-1))
+    {
+        return true ;
+    }
+    else{return false ;}
+}
+int main()
+{
+    int a[100];
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<issorted(a,n);
+
+}
