9cbccbe21e685af3aea4fe0481f5289d940bb6c5
parush2305 <parush.p2305@gmail.com>
2020-12-09 14:44:58 +0530

    easy

---
+++ 8_Lecture 5  Recursion 1/sum-of-array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int sum (int a[],int n)
+{
+    if(n==0){return 0;}
+    return a[0]+sum(a+1,n-1);
+}
+int main()
+{
+    int a[100];
+    int n;cin>>n;
+    for (int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<sum(a,n);
+}
